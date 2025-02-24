cfec046388472e8128445b69436638b457a53838
HADDANIAbdu <ab.haddani10@gmail.com>
2025-02-24 13:41:06 +0000

    Injection des dépendances en utilisant l'instanciation dynamique via le setter

--- src/main/java/ext/IDaoImplV2.java
+++ src/main/java/ext/IDaoImplV2.java
@@ -5,6 +5,7 @@ import dao.IDao;
 public class IDaoImplV2 implements IDao {
     @Override
     public double getData(){
+        System.out.println("web service Version !");
         double temperature = 67;
         return temperature;
     }
---
+++ src/main/java/present/PresentationV2.java
@@ -0,0 +1,33 @@
+package present;
+
+import dao.IDao;
+import metier.IMetier;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.util.Scanner;
+
+public class PresentationV2 {
+    public static void main(String[] args) {
+        try{
+            Scanner sc = new Scanner(new File("config.txt"));
+            String daoClassName = sc.nextLine();
+            Class cDao = Class.forName(daoClassName);
+            IDao Idao = (IDao) cDao.getConstructor().newInstance();
+
+            String metierClassName = sc.nextLine();
+            Class cMetier = Class.forName(metierClassName);
+            //utiliser le constructeur sans paramètre
+            IMetier Imetier = (IMetier) cMetier.getConstructor().newInstance();
+
+            //utiliser la méthode setIDao pour l'injection des dépendances
+            Method setIdao = cMetier.getDeclaredMethod("setIdao", IDao.class);
+            setIdao.invoke(Imetier, Idao);
+
+            System.out.println("Temperature : "+Imetier.calcul()+" °C");
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
