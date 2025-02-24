816ed932cb7d696e04a596b35a6a8e3d29bde7b4
HADDANIAbdu <ab.haddani10@gmail.com>
2025-02-24 12:43:32 +0000

    Injection des dépendances en utilisant l'instanciation statique  avec le constructeur

--- src/main/java/metier/IMetierImpl.java
+++ src/main/java/metier/IMetierImpl.java
@@ -4,6 +4,16 @@ import dao.IDao;
 public class IMetierImpl implements IMetier{
     private IDao Idao; //le couplage faible
 
+    public IMetierImpl() {
+        //constructeur sans paramètres pour utiliser le setter
+        //dans l'injection des dépendances
+    }
+
+    public IMetierImpl(IDao dao) {
+        this.Idao = dao;
+        //constructeur avec paramètre pour faire l'injection
+        //des dépendances avec le constructeur directement
+    }
     @Override
     public double calcul() {
         double temp =  Idao.getData();
--- src/main/java/present/PresentationV1.java
+++ src/main/java/present/PresentationV1.java
@@ -6,9 +6,13 @@ import metier.IMetierImpl;
 public class PresentationV1 {
     public static void main(String[] args) {
         IDaoImpl dao = new IDaoImpl();
-        IMetierImpl metier = new IMetierImpl();
-        metier.setIdao(dao);
-        System.out.println("Temperature : "+ metier.calcul() +" °C");
+        //DI avec le setter
+        //IMetierImpl metier = new IMetierImpl();
+        //metier.setIdao(dao);
+        //DI avec le constructeur
+        IMetierImpl metierCons = new IMetierImpl(dao);
+        //System.out.println("Temperature : "+ metier.calcul() +" °C");
+        System.out.println("Temperature constructeur : "+ metierCons.calcul() +" °C");
     }
 }
 
